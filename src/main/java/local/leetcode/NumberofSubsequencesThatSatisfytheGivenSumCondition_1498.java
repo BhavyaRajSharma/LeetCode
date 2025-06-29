1cb0eee263ffcd820fe29227d24ee385752ae59b
bhavyarajsharma <bhavyasharmacse17@gmail.com>
2025-06-30 01:06:37 +0530

    - LeetCode problem number 1498 -- Complexity: Medium -- Type: Array, Two pointer -- Category: Daily

---
+++ src/main/java/local/leetcode/NumberofSubsequencesThatSatisfytheGivenSumCondition_1498.java
@@ -0,0 +1,32 @@
+package local.leetcode;
+
+import java.util.Arrays;
+
+public class NumberofSubsequencesThatSatisfytheGivenSumCondition_1498 {
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int mod = 1_000_000_007;
+        int n = nums.length;
+
+        // Precompute powers of 2 up to n
+        int[] pow = new int[n];
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = (pow[i - 1] * 2) % mod;
+        }
+
+        int left = 0, right = n - 1;
+        int count = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                count = (count + pow[right - left]) % mod;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
